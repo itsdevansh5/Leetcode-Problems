9d940ca6341e30f3b923e7ea08aaf83382f6ad17
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-06-29 21:44:14 +0530

    Time: 0 ms (100%), Space: 8.5 MB (68.37%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,30 +1,18 @@
-#include<math.h>
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int count=1;
-        long int a=10;
-        int y=x;
-        while(x/a!=0){
-            count++;
-            a*=10;
+        // Negative numbers or numbers ending with 0 (but not 0 itself) can't be palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
 
-        }
-        long int b=10;
-        long int rev=0;
-        for(int i=count-1;i>=0;i--){
-            rev=rev+(x%10)*pow(b,i);
-            x=x/b;
+        int reversedHalf = 0;
 
-            
-
-        }
-        if (y>=0 and rev==y){
-            return true;
-        }
-        else{
-            return false;
+        // Reverse half the digits
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
         }
 
+        // For even and odd length numbers
+        return (x == reversedHalf || x == reversedHalf / 10);
     }
 };
