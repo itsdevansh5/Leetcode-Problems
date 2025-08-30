c0da784079731fbc869f8b407e1eae84c6874477
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-08-30 20:42:35 +0530

    Time: 0 ms (100%), Space: 8.5 MB (21.24%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        int n=s.size();
-        std::reverse(s.begin(),s.end());
-        string k=s;
-        for(int i=0;i<n;i++){
-                k=s;
-                std::reverse(k.begin(),k.begin()+i+1);
-                std::reverse(k.begin()+i+1,k.end());
-                if(k==goal)
-                return true;
-
-
-
-            
+        string start;
+        string end;
+        for(int i=0;i<s.size();i++){
+            if(s[i]==goal[0]){
+                 start=s.substr(i,s.size()-i);
+                 end=s.substr(0,s.size()-start.size());
+                  if(goal==(start+end))
+                 return true;
+            }
         }
+       
         return false;
         
     }
