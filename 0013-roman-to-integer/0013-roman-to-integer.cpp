941595e096bf6620242df716bcb5000f99657334
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-09-02 21:17:15 +0530

    Time: 17 ms (5.4%), Space: 13.3 MB (50.57%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,7 +1,3 @@
-#include <unordered_map>
-#include <string>
-using namespace std;
-
 class Solution {
 public:
     int romanToInt(string s) {
@@ -9,16 +5,14 @@ public:
             {'I',1}, {'V',5}, {'X',10}, {'L',50},
             {'C',100}, {'D',500}, {'M',1000}
         };
-        
-        int result = 0;
-        for (int i = 0; i < s.size(); ++i) {
-            int curr = roman[s[i]];
-            int next = (i+1 < s.size()) ? roman[s[i+1]] : 0;
-            
-            if (curr < next) {
-                result -= curr;
-            } else {
-                result += curr;
+    int result = 0;
+    for (int i = 0; i < s.size(); ++i) {
+    int curr = roman[s[i]];
+    int next = (i+1 < s.size()) ? roman[s[i+1]] : 0;
+    if (curr < next) {
+    result -= curr;
+    } else {
+        result += curr;
             }
         }
         return result;
