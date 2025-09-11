fa99a2b56429c43967f346d0c42a29d4d99b9a28
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-09-11 20:31:01 +0530

    Time: 6 ms (34.9%), Space: 124.7 MB (37.54%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,18 +10,31 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        vector<int>v;
-        ListNode *temp=head;
-        while(temp!=nullptr){
-            v.push_back(temp->val);
-            temp=temp->next;
-        }
-        vector<int>s;
-        s=v;
-        std::reverse(v.begin(),v.end());
-        return (v==s);
-
-        
+   bool isPalindrome(ListNode* head) {
+    if (!head) return true;
+    ListNode* temp = head;
+    int count = 0;
+    while (temp != nullptr) {
+        temp = temp->next;
+        count++;
     }
+    temp = head;
+    stack<int> s;
+    // Push first half values
+    for (int i = 0; i < count / 2; i++) {
+        s.push(temp->val);
+        temp = temp->next;
+    }
+    // If odd length, skip middle node
+    if (count % 2 != 0) temp = temp->next;
+    // Compare second half with stack
+    while (temp != nullptr) {
+        if (s.empty() || s.top() != temp->val)
+            return false;
+        s.pop();
+        temp = temp->next;
+    }
+    return s.empty();
+}
+
 };
