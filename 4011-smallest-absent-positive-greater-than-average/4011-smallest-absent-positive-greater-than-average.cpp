542c67b62c286491b42e50cddf0289dd87fc98c4
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-09-13 22:12:40 +0530

    Time: 16 ms (62.05%), Space: 92.5 MB (69.27%) - LeetHub

--- 4011-smallest-absent-positive-greater-than-average/4011-smallest-absent-positive-greater-than-average.cpp
+++ 4011-smallest-absent-positive-greater-than-average/4011-smallest-absent-positive-greater-than-average.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     int smallestAbsent(vector<int>& nums) {
-        int sum = 0;
-        unordered_set<int> freq;
-        int maxv = INT_MIN;
-
-        for (int i : nums) {
-            sum += i;
-            freq.insert(i);
-            maxv = max(maxv, i);
+        long long sum = 0;
+        int n = nums.size();
+        for(int i=0;i<n;i++) sum += nums[i];
+        long long avg = sum/n;
+        avg++;
+        unordered_set<int>s(nums.begin(),nums.end());
+        if(avg<=0) avg = 1;
+        while(1){
+            if(s.count(avg)==0) return avg;
+            avg++;
         }
-
-        double avg = static_cast<double>(sum) / nums.size();
-        int start = static_cast<int>(avg) + 1;
-        if (start < 1) start = 1;
-
-        for (int k = start; ; ++k) {
-            if (freq.find(k) == freq.end())
-                return k;
-        }
-
-        return 1; 
+        return -1;
     }
 };
