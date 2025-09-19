7cdf292a1bf59248015fd96e7b15a206d117fab6
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-09-19 18:31:56 +0530

    Time: 4 ms (24.46%), Space: 12.8 MB (60.26%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> indexMap;  
-        vector<int> ans;
-
-        for(int i=0; i<nums2.size(); i++) {
-            indexMap[nums2[i]] = i;
+        vector<int>ans;
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums2.size();i++){
+            mp[nums2[i]]=i;
         }
-
-    
-        for(int i=0; i<nums1.size(); i++) {
-            int startIdx = indexMap[nums1[i]];
-            bool found = false;
-
-        
-            for(int j=startIdx+1; j<nums2.size(); j++) {
-                if(nums2[j] > nums1[i]) {
-                    ans.push_back(nums2[j]);
-                    found = true;
-                    break;
+        for(auto i:nums1){
+            bool inserted=false;
+            for(int j=mp[i]+1;j<nums2.size();j++){
+                if(nums2[j]>i){
+                ans.push_back(nums2[j]);
+                inserted=true;
+                break;
                 }
             }
-
-            if(!found) {
-                ans.push_back(-1);
-            }
+            if(!inserted)
+            ans.push_back(-1);
+            
         }
-
         return ans;
+        
+        
     }
 };
