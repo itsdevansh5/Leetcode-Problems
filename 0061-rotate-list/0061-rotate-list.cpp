b44793f5202080561f40079320bc39f76f1ed2bb
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-09-21 22:06:45 +0530

    Time: 0 ms (100%), Space: 16.5 MB (31.09%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -17,7 +17,6 @@ public:
 
         if (m == 0 || t == nullptr) return t;
         
-        // This loop will reverse 'm' nodes starting from 't'
         for (int i = 0; i < m && curr != nullptr; ++i) {
             next_node = curr->next;
             curr->next = prev;
@@ -25,11 +24,7 @@ public:
             curr = next_node;
         }
         
-        // 'prev' is the new head of the reversed sublist, but 't' is now its tail.
-        // We need to link the reversed sublist back to the rest of the list.
-        // 'curr' holds the head of the un-reversed part of the list.
-        // 't->next = curr;' is the missing link. But since we return the head of the reversed list,
-        // we need to handle this in rotateRight.
+
         
         return prev;
     }
@@ -37,7 +32,7 @@ public:
     ListNode* rotateRight(ListNode* head, int k) {
         if(head == nullptr || head->next == nullptr) return head;
         
-        // 1. Find the total number of nodes
+    
         int n = 1;
         ListNode * tail = head;
         while(tail->next != nullptr){
@@ -45,22 +40,15 @@ public:
             n++;
         }
         
-        // 2. Calculate the effective rotation
         k = k % n;
         if (k == 0) return head;
-
-        // 3. Find the split point
-        // The new head will be the (n-k)-th node from the original head
         ListNode* new_tail = head;
         for (int i = 0; i < n - k - 1; ++i) {
             new_tail = new_tail->next;
         }
         
-        // 4. Set the new head and detach the list
         ListNode* new_head = new_tail->next;
         new_tail->next = nullptr;
-        
-        // 5. Connect the original tail to the original head
         tail->next = head;
 
         return new_head;
