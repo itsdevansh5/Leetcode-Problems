0d0442c17c921a967d44a790f239de31598496e7
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-09-29 21:20:17 +0530

    Time: 0 ms (100%), Space: 9 MB (60.81%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string output;
+         int i=a.size()-1;
+         int j=b.size()-1;
+         int m,n,sum;
+         int carry=0;
+         while(i>=0 || j>=0){
+            if(i<0)m=0;
+            else m=a[i]-'0';
+            if(j<0)n=0;
+            else n=b[j]-'0';
+            sum=m+n+carry;
+            if(sum==2){
+                carry=1;
+                sum=0;
+            }
+            else if(sum==3){
+                carry=1;
+                sum=1;
+            }
+            else{
+                carry=0;
+            }
+           output.insert(output.begin(),'0'+sum);
+           i--;
+           j--;
+            
+
+
+         }
+         if(carry)
+         output.insert(output.begin(),'1');
+         return output;
+        
+    }
+};
