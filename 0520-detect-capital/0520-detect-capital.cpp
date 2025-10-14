d78479f0c81bfb62f69f61a5088916e3969aee94
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-10-14 19:24:20 +0530

    Time: 2 ms (6%), Space: 8 MB (80.3%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,25 +1,10 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        if(isupper(word[0])){
-            int count=0;
-            for(int i=1;i<word.size();i++){
-                if(isupper(word[i]))
-                count++;
-            }
-            if(count==0 || count==word.size()-1)
-            return true;
-            else
-            return false;
-
-        }
-        else{
-            for(int i=1;i<word.size();i++){
-                if(isupper(word[i]))
-                return false;
-            }
-            return true;
-        }
-        
+        int cnt = 0;
+        for (char c : word)
+            if (isupper(c))
+                ++cnt;
+        return cnt == 0 || cnt == word.size() || (cnt == 1 && isupper(word[0]));
     }
 };
