463032fc368274fe689c681a3bbd7e1248f9cd07
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-10-27 07:30:55 +0530

    Time: 0 ms (100%), Space: 7.9 MB (72.94%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int next(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = next(n);
+        while (fast != 1 && slow != fast) {
+            slow = next(slow);
+            fast = next(next(fast));
+        }
+        return fast == 1;
+    }
+};
