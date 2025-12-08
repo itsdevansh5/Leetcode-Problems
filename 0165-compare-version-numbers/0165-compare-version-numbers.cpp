e61005d70ce4dd078f36fd294f44d8445e40e12c
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-12-08 18:21:16 +0530

    Time: 0 ms (100%), Space: 8.2 MB (84.3%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-         vector<int>v1;
-         vector<int>v2;
-         stringstream s1(version1);
-         stringstream s2(version2);
-         string ver;
-         while(getline(s1,ver,'.')){
-            int a=stoi(ver);
-            v1.push_back(a);
-         }
-         while(getline(s2,ver,'.')){
-            int b=stoi(ver);
-            v2.push_back(b);
-         }
-         for(int i=0;i<max(v1.size(),v2.size());i++){
-            int m,n;
-            if(i>=v1.size())
-            m=0;
-            else
-            m=v1[i];
-            if(i>=v2.size())
-            n=0;
-            else
-            n=v2[i];
+        int i=0;
+        int j=0;
+        int m=version1.size();
+        int n=version2.size();
+        while(i<m || j<n){
+            int num1=0;
+            int num2=0;
+            while(i<m && version1[i]!='.'){
+                num1=num1*10+(version1[i]-'0');
+                i++;
+            }
+            i++;
+            while(j<n && version2[j]!='.'){
+                num2=num2*10+(version2[j]-'0');
+                j++;
 
-            if(m==n)
-            continue;
-            else if(m>n)
-            return 1;
-            else
-            return -1;
-         }
-         return 0;
+            }
+            j++;
+            if(num1>num2) return 1;
+            if(num1<num2) return -1;
+
+
+
+        }
+        return 0;
+
     }
 };
