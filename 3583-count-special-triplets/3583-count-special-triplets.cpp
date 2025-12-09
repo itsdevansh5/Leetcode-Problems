0cb6e00dae7506af377e7b9cc6a599c7b461345c
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-12-09 15:38:25 +0530

    Time: 932 ms (31.02%), Space: 428 MB (25.55%) - LeetHub

---
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int specialTriplets(vector<int>& nums) {
+        long long mod = 1e9 + 7;
+
+        unordered_map<int,int> right;
+        for (int x : nums)
+            right[x]++;
+
+        unordered_map<int,int> left;
+        long long count = 0;
+
+        for (int x : nums) {
+            right[x]--;
+
+            long long a = left[2 * x]; 
+            long long b = right[2 * x]; 
+
+            count = (count + (a * b) % mod) % mod;
+
+            left[x]++;
+        }
+
+        return count;
+    }
+};
