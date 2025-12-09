8fdfa188633700399d71629cd2b2abef2a5b093b
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-12-09 20:13:51 +0530

    Time: 5 ms (14.92%), Space: 10.8 MB (30.49%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        stack<char>st;
-        set<int>m;
-        set<char>v={'a', 'e', 'i', 'o','u','A','E','I','O','U'};
-        for(int i=0;i<s.size();i++){
-            if(v.find(s[i])!=v.end()){
-            st.push(s[i]);
-            m.insert(i);
-            }
-            
-        }
-        for(int i=0;i<s.size();i++){
-            if(m.find(i)!=m.end()){
-                s[i]=st.top();
-                st.pop();
+        int i=0;
+        int j=s.size()-1;
+        set<char>v={ 'a', 'e', 'i', 'o','u','A','E','I','O','U'};
+        while(i<j){
+            bool a=false;
+            bool b=false;
+            if(v.find(s[i])!=v.end())
+            a=true;
+            else
+            i++;
+
+            if(v.find(s[j])!=v.end())
+            b=true;
+            else
+            j--;
+
+            if(a && b){
+            std::swap(s[i],s[j]);
+            i++;
+            j--;
             }
         }
         return s;
-        
     }
 };
