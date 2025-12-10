43f75997f957e4045224cebff1dab8296bf96b3f
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-12-10 19:21:03 +0530

    Time: 71 ms (10.22%), Space: 34.3 MB (11.05%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
-    bool anagram(string a,string b){
-        vector<int>v(26,0);
-        for(auto i:a){
-            v[i-'a']++;
-        }
-        for(auto i:b){
-            v[i-'a']--;
-        }
-        for(auto i:v){
-            if(i!=0)
-            return false;
-        }
-        return true;
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
 
-    }
-   vector<vector<string>> groupAnagrams(vector<string>& strs) {
-    int n = strs.size();
-    vector<bool> used(n, false);
-    vector<vector<string>> ans;
-
-    for (int i = 0; i < n; i++) {
-        if (used[i]) continue;
-
-        vector<string> group;
-        group.push_back(strs[i]);
-        used[i] = true;
-
-        for (int j = i + 1; j < n; j++) {
-            if (!used[j] && strs[i].size() == strs[j].size() && anagram(strs[i], strs[j])) {
-                used[j] = true;
-                group.push_back(strs[j]);
+        for (auto &s : strs) {
+            // Build frequency key
+            vector<int> freq(26, 0);
+            for (char c : s) {
+                freq[c - 'a']++;
             }
-        }
-        ans.push_back(group);
-    }
 
-    return ans;
-}
+            // Convert vector<int> → string key
+            string key;
+            for (int x : freq) {
+                key += to_string(x) + '#';
+            }
+
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+        for (auto &p : mp) {
+            ans.push_back(p.second);
+        }
+        return ans;
+    }
 };
