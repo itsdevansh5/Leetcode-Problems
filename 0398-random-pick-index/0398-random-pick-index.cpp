4c0ab6c70d9913679232f3959ba425c2ebfa679d
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-12-12 13:53:13 +0530

    Time: 28 ms (97.59%), Space: 66.7 MB (55.92%) - LeetHub

--- 0398-random-pick-index/0398-random-pick-index.cpp
+++ 0398-random-pick-index/0398-random-pick-index.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
-    
-uint32_t fastRand() {
-    static uint32_t x = 0xabcdef01;
-    x ^= x << 13;
-    x ^= x >> 17;
-    x ^= x << 5;
-    return x;
-}
-    vector<int>arr;
-    unordered_map<int,vector<int>>mp;
+
+    uint32_t fastRand() {
+        static uint32_t x = 0xabcdef01;
+        x ^= x << 13;
+        x ^= x >> 17;
+        x ^= x << 5;
+        return x;
+    }
+
+    unordered_map<int, vector<int>> mp;
+
     Solution(vector<int>& nums) {
-        this->arr =nums;
-        for(int i=0;i<this->arr.size();i++){
-           if(mp.find(arr[i])==mp.end())
-           mp[arr[i]]={i};
-           else
-           mp[arr[i]].push_back(i);
+        for (int i = 0; i < nums.size(); i++) {
+            mp[nums[i]].push_back(i);
         }
     }
     
     int pick(int target) {
-        vector<int>indx=mp[target];
-        int ans=fastRand() % indx.size();
-        return indx[ans];
-        
+        const vector<int>& indx = mp[target]; 
+        int pos = fastRand() % indx.size();    
+        return indx[pos];
     }
 };
-
-/**
- * Your Solution object will be instantiated and called as such:
- * Solution* obj = new Solution(nums);
- * int param_1 = obj->pick(target);
- */
