1099a77d5c1fd1c66fdf6f1d17b5f3d22a56dd7e
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-12-14 16:06:51 +0530

    Time: 6 ms (60.25%), Space: 15.9 MB (92.01%) - LeetHub

--- 0811-subdomain-visit-count/0811-subdomain-visit-count.cpp
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.cpp
@@ -3,17 +3,18 @@ public:
     vector<string> subdomainVisits(vector<string>& cpdomains) {
         unordered_map<string,int>mp;
          for(int i=0;i<cpdomains.size();i++){
+              string fulld=cpdomains[i];
               int num=0;
               int k=0;
-              while(k<cpdomains[i].size() && isdigit(cpdomains[i][k])){
-                num=num*10+(cpdomains[i][k]-'0');
+              while(k<fulld.size() && isdigit(fulld[k])){
+                num=num*10+(fulld[k]-'0');
                 k++;
               }
             int j=k+1;
-            while(j<cpdomains[i].size()){
-                 string subd=cpdomains[i].substr(j);
+            while(j<fulld.size()){
+                 string subd=fulld.substr(j);
                  mp[subd]+=num;
-                 while(j<cpdomains[i].size() && cpdomains[i][j]!='.'){
+                 while(j<fulld.size() && fulld[j]!='.'){
                     j++;
                  }
                  j++;
