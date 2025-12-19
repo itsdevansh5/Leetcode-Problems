fa40d76ac141f098918e0d110e6a114c525a7447
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-12-19 13:53:56 +0530

    Time: 160 ms (37.98%), Space: 46.2 MB (91.52%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     string replaceWords(vector<string>& dictionary, string sentence) {
-        unordered_set<string>dict(dictionary.begin(),dictionary.end());
+        unordered_set<string> dict(dictionary.begin(), dictionary.end());
         string output;
-        int i=0;
-        while(i<sentence.size()){
-           string word;
-           while(i<sentence.size() && sentence[i]!=' '){
-              word+=sentence[i];
-              if(dict.find(word)!=dict.end()){
-              break;
-              }
-              i++;
-           }
-           output+=word;
-           while(i<sentence.size() && sentence[i]!=' '){
-            i++;
-           }
-           i++;
-           if(i<sentence.size())
-           output+=' ';
+        int i = 0;
 
-         
+        while (i < sentence.size()) {
+            string word;
+
+            while (i < sentence.size() && sentence[i] != ' ') {
+                word += sentence[i];
+                i++;
+
+                if (dict.count(word)) {
+                    break;
+                }
+            }
+
+            
+            while (i < sentence.size() && sentence[i] != ' ') {
+                i++;
+            }
+
+            output += word;
+
+            if (i < sentence.size()) {
+                output += ' ';
+                i++;
+            }
         }
+
         return output;
-        
     }
 };
