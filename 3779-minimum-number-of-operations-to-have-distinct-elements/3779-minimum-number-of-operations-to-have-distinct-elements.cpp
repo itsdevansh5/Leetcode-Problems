81762c344ea29298d7e30191ac43515853de00f1
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-12-23 12:46:07 +0530

    Time: 60 ms (72.53%), Space: 186.1 MB (88.31%) - LeetHub

--- 3779-minimum-number-of-operations-to-have-distinct-elements/3779-minimum-number-of-operations-to-have-distinct-elements.cpp
+++ 3779-minimum-number-of-operations-to-have-distinct-elements/3779-minimum-number-of-operations-to-have-distinct-elements.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        unordered_set<int>s;
+        unordered_set<int> s;
         int i;
-        for( i=nums.size()-1;i>=0;i--){
-            if(s.find(nums[i])==s.end())
+
+        for (i = nums.size() - 1; i >= 0; i--) {
+            if (s.count(nums[i]))
+                break;
             s.insert(nums[i]);
-            else
-            break;
         }
-        int count=i-0+1;;
-        count=(int)ceil(count/3.0);
-        return count;
+
+        int prefix = i + 1;          
+        return (prefix + 2) / 3;     
     }
 };
