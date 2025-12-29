c15f068a090a8d7312507acee2bb990f09b52425
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-12-29 18:08:32 +0530

    Time: 227 ms (5.5%), Space: 245.4 MB (10%) - LeetHub

---
+++ 3788-maximum-score-of-a-split/3788-maximum-score-of-a-split.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long maximumScore(vector<int>& nums) {
+        long long presum=0;
+        long long maxscore=LLONG_MIN;
+        unordered_map<int,long long>mp;
+        long long sufsum=LLONG_MAX;
+        for(int i=nums.size()-1;i>=0;i--){
+            if(nums[i]<sufsum)
+            sufsum=nums[i];
+            mp[i]=sufsum;
+        }
+        for(int i=0;i<nums.size()-1;i++){
+            presum+=nums[i];
+            long long diff;
+             diff=presum-mp[i+1];
+            if(diff>maxscore)
+            maxscore=diff;
+
+
+        }
+        return maxscore;
+    }
+};
