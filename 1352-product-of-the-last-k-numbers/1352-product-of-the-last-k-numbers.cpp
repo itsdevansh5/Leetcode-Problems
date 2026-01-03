36f90611f50cc19839800fb16e8833653434562c
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-01-03 14:25:55 +0530

    Time: 1033 ms (12.47%), Space: 98.3 MB (34.4%) - LeetHub

---
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
@@ -0,0 +1,28 @@
+class ProductOfNumbers {
+public:
+   vector<int>stream;
+    ProductOfNumbers() {
+        
+    }
+    
+    void add(int num) {
+        stream.push_back(num);
+    }
+    
+    int getProduct(int k) {
+        int prod=1;
+        int n=stream.size()-1;
+        while(k--){
+            prod*=stream[n];
+            n--;
+        }
+        return prod;
+    }
+};
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
