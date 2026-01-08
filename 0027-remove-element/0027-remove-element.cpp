83559d26863c62f25988056353fba37b1642a5c4
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-01-08 21:57:36 +0530

    Time: 1 ms (2.22%), Space: 11.8 MB (10.18%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,16 +1,13 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int write = 0;
-        for (int read = 0; read < nums.size(); read++) {
-            if (nums[read] != val) {
-                nums[write] = nums[read];
-                write++;
+        int slow=0;
+        for(int fast=0;fast<nums.size();fast++){
+            if(nums[fast]!=val){
+                nums[slow]=nums[fast];
+                slow++;
             }
         }
-        return write;
+        return slow;
     }
 };
