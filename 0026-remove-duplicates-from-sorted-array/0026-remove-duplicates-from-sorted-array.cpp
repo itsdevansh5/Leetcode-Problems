47c8e88f16873feb5e4311ad0484995bc5e9fc54
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-01-08 20:20:25 +0530

    Time: 0 ms (100%), Space: 22.8 MB (18%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,27 +1,19 @@
-#include <vector>
-
 class Solution {
 public:
-    int removeDuplicates(std::vector<int>& nums) {
-        // Handle edge case of an empty or single-element vector
-        if (nums.size() <= 1) {
-            return nums.size();
+    int removeDuplicates(vector<int>& nums) {
+        int slow=0;
+        int fast=1;
+       while(fast<nums.size()){
+        while(fast<nums.size() && nums[slow]==nums[fast]){
+            fast++;
         }
-
-        int i = 0;
-        // Loop while 'i' is not the last element, to safely check i+1
-        while (i < nums.size() - 1) {
-            // Check if the current element is a duplicate of the next one
-            if (nums[i] == nums[i + 1]) {
-                // Erase the duplicate element (at i + 1)
-                nums.erase(nums.begin() + (i + 1));
-            } else {
-                // If it's not a duplicate, move to the next element
-                i++;
-            }
+        if(slow+1<nums.size() && fast<nums.size()){
+        nums[slow+1]=nums[fast];
+        slow++;
         }
         
-        // Return the new size of the vector after all duplicates have been removed
-        return nums.size();
+       }
+       return slow+1;
+
     }
 };
