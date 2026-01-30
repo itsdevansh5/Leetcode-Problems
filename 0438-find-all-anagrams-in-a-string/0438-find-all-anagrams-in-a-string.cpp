9c39774aaffe83d3f33167362a411efea614f7a8
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-01-30 23:02:30 +0530

    Time: 3 ms (79.62%), Space: 11.6 MB (36.5%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-    
+        vector<int> freq(26, 0);
+        for (char c : p) freq[c - 'a']++;
 
-        vector<int>freq(26,0);
-        vector<int>org(26,0);
-        vector<int>res;
-        for(auto i:p) freq[i-'a']++;
-        int i=0;
-        int n=p.size();
-        int need=p.size();
-        for(int j=0;j<s.size();j++){
-            org[s[j]-'a']++;
-          if(freq[s[j]-'a']>=org[s[j]-'a']) need--;
+        vector<int> res;
+        int need = p.size();
+        int i = 0;
 
-          while((j-i+1)>n){
-            if(freq[s[i]-'a']>=org[s[i]-'a']) need++;
-            org[s[i]-'a']--;
-            i++;
-          }
-          if((j-i+1)==n && need==0) res.push_back(i);
+        for (int j = 0; j < s.size(); j++) {
+            if (freq[s[j] - 'a'] > 0)
+                need--;
+            freq[s[j] - 'a']--;
+
+            while (j - i + 1 > p.size()) {
+                freq[s[i] - 'a']++;
+                if (freq[s[i] - 'a'] > 0)
+                    need++;
+                i++;
+            }
+
+            if (need == 0)
+                res.push_back(i);
         }
         return res;
     }
