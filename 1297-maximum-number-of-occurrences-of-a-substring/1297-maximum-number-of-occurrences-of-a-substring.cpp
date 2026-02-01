66e45a24930ef2db761f92bb522a7e3b63ec9f15
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-02-01 16:00:44 +0530

    Time: 27 ms (89.61%), Space: 15.9 MB (83.92%) - LeetHub

--- 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.cpp
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int maxFreq(string s, int maxLetters, int minSize, int maxSize) {
-        vector<std::pair<int,int>>subs;
         int i=0;
         int j=0;
+        unordered_map<string,int>strings;
         vector<int>freq(26,0);
         int letters=0;
         for(int j=0;j<s.size();j++){
@@ -16,16 +16,11 @@ public:
                 freq[r-'a']--;
                 if(freq[r-'a']==0) letters--;
             }
-            if((j-i+1)==minSize)
-           subs.push_back({i,j});
+            if ((j - i + 1) == minSize)
+    strings[s.substr(i, minSize)]++;
         }
-        unordered_map<string,int>strings;
-        for(int i=0;i<subs.size();i++){
-            int a=subs[i].first;
-            int b=subs[i].second;
-            string sub=s.substr(a,(b-a+1));
-            strings[sub]++;
-        }
+        
+        
         int maxv=0;
         for(auto i:strings){
 if(i.second>maxv){
