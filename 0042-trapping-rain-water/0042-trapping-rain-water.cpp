ad034126e331e694448490b3b78ca9e9f11d5d8b
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-02-07 12:14:20 +0530

    Time: 0 ms (100%), Space: 28.1 MB (5.14%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        unordered_map<int,pair<int,int>>mp;
+        vector<int>left;
+        vector<int>right(height.size());
         int maxv=0;
         for(int i=0;i<height.size();i++){
-          mp[i].first=maxv;
+          left.push_back(maxv);
           maxv=max(maxv,height[i]);
         }
         maxv=0;
         for(int j=height.size()-1;j>=0;j--){
-           mp[j].second=maxv;
+           right[j]=maxv;
            maxv=max(maxv,height[j]);
         }
         int tot=0;
         for(int i=0;i<height.size();i++){
             int m,n;
-                m=mp[i].first;
-                n=mp[i].second;
+                m=left[i];
+                n=right[i];
                 int width=min(m,n);
                 
                 int wat=width-(height[i]);
