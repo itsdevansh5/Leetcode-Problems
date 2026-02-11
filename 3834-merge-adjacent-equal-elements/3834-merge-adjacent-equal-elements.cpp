b1a8dec4f14ee3def58e329498d44945f290db7c
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-02-11 20:43:38 +0530

    Time: 9 ms (89.09%), Space: 198.9 MB (88.34%) - LeetHub

--- 3834-merge-adjacent-equal-elements/3834-merge-adjacent-equal-elements.cpp
+++ 3834-merge-adjacent-equal-elements/3834-merge-adjacent-equal-elements.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    vector<long long> mergeAdjacent(vector<int>& nums) {
-        vector<long long>ans;
-        stack<long long>st;
-        for(int i=0;i<nums.size();i++){
-            long long r=nums[i];
-              while(!st.empty() && st.top()==r){
-                long long  a=st.top();
-                st.pop();
-                r=r+a;
-              }
-          st.push(r);
+    vector<long long> mergeAdjacent(vector<int>& a) {
+        vector<long long> ans;
+        for (int i = 0; i < a.size(); i++) {
+            long long curr = a[i];
+            while (ans.size() && ans.back() == curr) {
+                ans.pop_back();
+                curr *= 2;
+            }
+            ans.push_back(curr);
         }
-        while(!st.empty()){
-            long long a=st.top();
-            ans.push_back(a);
-            st.pop();
-        }
-        std::reverse(ans.begin(),ans.end());
         return ans;
     }
 };
