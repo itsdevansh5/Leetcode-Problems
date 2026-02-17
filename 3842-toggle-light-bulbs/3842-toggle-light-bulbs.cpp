9d55f8c813a6176924cefc43edeb4bcafed175b6
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-02-18 00:29:25 +0530

    Time: 3 ms (62.09%), Space: 33.5 MB (75.36%) - LeetHub

--- 3842-toggle-light-bulbs/3842-toggle-light-bulbs.cpp
+++ 3842-toggle-light-bulbs/3842-toggle-light-bulbs.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> toggleLightBulbs(vector<int>& bulbs) {
-        vector<bool>arr(100,false);
+        vector<int>arr(100,0);
         for(auto i:bulbs){
-            arr[i-1]=!arr[i-1];
+           arr[i-1]++;
 
         }
         vector<int>res;
         for(int i=0;i<100;i++){
-            if(arr[i]==true) res.push_back(i+1);
+            if(arr[i]%2!=0) res.push_back(i+1);
         }
         return res;
     }
