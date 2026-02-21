f093053ec1a9298339f25daf89eb02962e393822
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-02-21 14:52:31 +0530

    Time: 3 ms (27.06%), Space: 8.7 MB (86.7%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-      vector<int>a(26,0);
-      vector<int>b(26,0);
+      vector<int>li(26,0);
       vector<int>res;
-      for(auto i:s){
-        a[i-'a']++;
-      }
-      int act=0;
-      int valid=0;
-      int last=0;
       for(int i=0;i<s.size();i++){
-        if(b[s[i]-'a']==0) act++;
-          b[s[i]-'a']++;
-          if(b[s[i]-'a']==a[s[i]-'a']) valid++;
-
-          if(valid==act){
-            valid=0;
-            act=0;
-            if(res.size()==0)
-            res.push_back(i-last+1);
-            else 
-            res.push_back(i-last);
-            last=i;
-          }
+        li[s[i]-'a']=i;
+      }
+      int curlast=-1;
+      int start=0;
+      for(int i=0;i<s.size();i++){
+        curlast=max(curlast,li[s[i]-'a']);
+        if(curlast==i){
+           
+            res.push_back(i-start+1);
+             start=i+1;
+        }
 
       }
       return res;
