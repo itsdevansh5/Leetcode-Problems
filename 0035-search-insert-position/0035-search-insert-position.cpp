b88aafb4b18e65d8e8999217f6b6d12a4be25e47
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-02-22 20:18:04 +0530

    Time: 0 ms (100%), Space: 13.7 MB (42.33%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int l=0;
-        int h=nums.size()-1;
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(nums[mid]==target)
-            return mid;
-            else if(nums[mid]>target)
-            h=mid-1;
-            else
-            l=mid+1;
-        }
-        auto it=std::upper_bound(nums.begin(),nums.end(),target);
-        return distance(nums.begin(),it);
-        
+     int l=0;
+     int h=nums.size();
+     while(l<h){
+        int mid=l+(h-l)/2;
+        if(nums[mid]>=target) h=mid;
+        else l=mid+1;
+
+     }  
+     return l; 
     }
 };
