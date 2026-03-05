049ac304e49f53276b932bace7760f154aee4ba3
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-03-06 01:52:44 +0530

    Time: 223 ms (5.06%), Space: 36.3 MB (6.44%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
@@ -3,24 +3,26 @@ public:
     int longestBeautifulSubstring(string word) {
         deque<char>dq;
         unordered_map<char,int>mp;
-        unordered_map<char,int>np;
-        mp['a']=1;
-        mp['e']=2;
-        mp['i']=3;
-        mp['o']=4;
-        mp['u']=5;
+        vector<int>np(5,0);
+        mp['a']=0;
+        mp['e']=1;
+        mp['i']=2;
+        mp['o']=3;
+        mp['u']=4;
         int maxl=0;
+        int count=0;
         for(int i=0;i<word.size();i++){
           while(!dq.empty() && mp[dq.back()]>mp[word[i]]){
             char r=dq.front();
             dq.pop_front();
-            np[r]--;
-            if(np[r]==0) np.erase(r);
+            np[mp[r]]--;
+            if(np[mp[r]]==0) count--;
           }
           dq.push_back(word[i]);
-          np[word[i]]++;
+          if(np[mp[word[i]]]==0) count++;
+          np[mp[word[i]]]++;
           int s=dq.size();
-          if(np.size()==5)
+          if(count==5)
           maxl=max(s,maxl);
         }
         return maxl;
