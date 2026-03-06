e88b163a2a58086c2792ee30f0e954364318f11f
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-03-06 13:44:07 +0530

    Time: 85 ms (19.01%), Space: 146.5 MB (24.81%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -3,11 +3,11 @@ public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         unordered_map<int,int>rem;
         int cursum=0;
+        rem[0]=-1;
         for(int i=0;i<nums.size();i++){
             cursum+=nums[i];
             int r=cursum%k;
-            if(r==0 && i>=1) return true;
-            else if(rem.find(r)!=rem.end() && (i-rem[r]>=2)) return true;
+             if(rem.find(r)!=rem.end() && (i-rem[r]>=2)) return true;
             if(rem.find(r)==rem.end())
             rem[r]=i;
         }
