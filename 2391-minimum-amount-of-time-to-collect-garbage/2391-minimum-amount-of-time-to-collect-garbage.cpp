5e3ba92787ddffe1ba0cdea60f1aed894b8df3db
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-03-12 23:43:20 +0530

    Time: 18 ms (55.56%), Space: 108.6 MB (20.39%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
-        int g=0;
-        int p=0;
-        int m=0;
-        int lg=0;
-        int lm=0;
-        int lp=0;
-        for(int i=0;i<garbage.size();i++){
-         string &house= garbage[i];
-           for(int j=0;j<house.size();j++){
-                  if(house[j]=='G'){
+        int n = garbage.size();
+        
+        int g = 0, p = 0, m = 0;
+        int lg = 0, lp = 0, lm = 0;
+
+        for(int i = 0; i < n; i++){
+            for(char c : garbage[i]){
+                if(c == 'G'){
                     g++;
-                    lg=i;
-                  }
-                  else if(house[j]=='P') {
-                     p++;
-                     lp=i;
-                  }
-                  else {
-                 m++;
-                 lm=i;
-                  }
-           }
+                    lg = i;
+                }
+                else if(c == 'P'){
+                    p++;
+                    lp = i;
+                }
+                else{
+                    m++;
+                    lm = i;
+                }
+            }
         }
-        int tr=0;
-        for(int i=1;i<=lm;i++) tr+=(travel[i-1]);
-         for(int i=1;i<=lp;i++) tr+=(travel[i-1]);
-          for(int i=1;i<=lg;i++) tr+=(travel[i-1]);
-        return (p+g+m+tr);
+
+        // prefix travel time
+        vector<int> prefix(n,0);
+        for(int i = 1; i < n; i++){
+            prefix[i] = prefix[i-1] + travel[i-1];
+        }
+
+        int totalTravel = prefix[lg] + prefix[lp] + prefix[lm];
+
+        return g + p + m + totalTravel;
     }
 };
