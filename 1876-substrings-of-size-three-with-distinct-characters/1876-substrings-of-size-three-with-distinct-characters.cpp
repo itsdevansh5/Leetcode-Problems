29a86e213652bff5a2d1471a0a74302689caad17
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-03-14 16:37:59 +0530

    Time: 0 ms (100%), Space: 8.3 MB (71.72%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int countGoodSubstrings(string s) {
-        vector<int>freq(26,0);
-        int i=0;
-        int count=0;
-        for(int j=0;j<s.size();j++){
-            freq[s[j]-'a']++;
+        int count = 0;
+        if(s.size()<3) return 0;
 
-            while((j-i+1)>3){
-                freq[s[i]-'a']--;
-                i++;
-            }
-
-            while(freq[s[j]-'a']>1){
-                freq[s[i]-'a']--;
-                i++;
-            }
-
-            if((j-i+1)==3) count++;
-
+        for(int i = 0; i < s.size() - 2; i++){
+            if(s[i] != s[i+1] &&
+               s[i] != s[i+2] &&
+               s[i+1] != s[i+2])
+                count++;
         }
+
         return count;
     }
 };
