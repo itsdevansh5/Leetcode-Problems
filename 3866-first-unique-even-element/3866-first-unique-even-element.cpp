a882171ad6bad93cfbf683c607963041dd9533f5
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-03-16 22:21:57 +0530

    Time: 0 ms (100%), Space: 28.1 MB (40%) - LeetHub

--- 3866-first-unique-even-element/3866-first-unique-even-element.cpp
+++ 3866-first-unique-even-element/3866-first-unique-even-element.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int firstUniqueEven(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(int i:nums){
-            mp[i]++;
+        vector<int>arr(101,0);
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0) arr[nums[i]]++;
         }
         for(int i=0;i<nums.size();i++){
-            if(nums[i]%2==0 && mp[nums[i]]==1) return nums[i];
+            if(nums[i]%2==0 && arr[nums[i]]==1) return nums[i];
         }
         return -1;
     }
