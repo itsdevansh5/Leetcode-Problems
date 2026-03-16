168e473a444bcab672609961f4a96dea17f0c837
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-03-16 18:12:46 +0530

    Time: 0 ms (100%), Space: 8.8 MB (28.57%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int ndigits(int n){
+        int res=0;
+        while(n>0){
+            res++;
+            n/=10;
+        }
+        return res;
+    }
+    int countCommas(int n) {
+        int nd=ndigits(n);
+        int r=nd/3;
+    string s="999";
+    int res=0;
+    while(r--){
+        int t=n-stoi(s);
+        if(t>0)
+        res+=t;
+        s+=s;
+    }
+    return res;
+    }
+    
+};
