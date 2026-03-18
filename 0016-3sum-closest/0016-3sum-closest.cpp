652b0cad5062d56bf1b4954759b14180bf5139e2
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-03-19 01:11:55 +0530

    Time: 11 ms (61.14%), Space: 14 MB (32.89%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -6,6 +6,7 @@ public:
         int mins=INT_MAX;
         int res;
         for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
             int l=i+1;
             int r=n-1;
             int cur;
