a643eea7ef7a6211df6240b727d9c427c256100d
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-03-19 01:13:13 +0530

    Time: 15 ms (32.02%), Space: 14.1 MB (32.89%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        std::sort(nums.begin(),nums.end());
-        int n=nums.size();
-        int mins=INT_MAX;
-        int res;
-        for(int i=0;i<n-2;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            int l=i+1;
-            int r=n-1;
-            int cur;
-            while(l<r){
-             cur=nums[i]+nums[l]+nums[r];
-              if(cur>target) r--;
-              else if(cur<target) l++;
-              else return target;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int res = nums[0] + nums[1] + nums[2];
 
-              if(mins>abs(cur-target)){
-                mins=abs(cur-target);
-                res=cur;
-              }
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            int l = i + 1, r = n - 1;
 
-              
-              
+            while (l < r) {
+                int cur = nums[i] + nums[l] + nums[r];
+
+                if (abs(cur - target) < abs(res - target))
+                    res = cur;
+
+                if (cur == target)     return target;
+                else if (cur < target) l++;
+                else                   r--;
             }
-           
         }
         return res;
     }
