2f4a2e9da87a7670e2119ec0a3bd20c50ff1d4d7
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-03-25 12:47:53 +0530

    Time: 1 ms (68.46%), Space: 11.4 MB (35.01%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -3,16 +3,16 @@ public:
     string frequencySort(string s) {
         vector<int>freq(256,0);
         for(char c:s){
-            freq[c]++;
+            freq[(unsigned char)c]++;
         }
        vector<char>ch;
        for(int i=0;i<freq.size();i++){
         if(freq[i]>0) ch.push_back(char(i));
        }
-       std::sort(ch.begin(),ch.end(),[&](char a,char b){return freq[a]>freq[b];});
+       std::sort(ch.begin(),ch.end(),[&](char a,char b){return freq[(unsigned char)a]>freq[(unsigned char)b];});
        string ans;
        for(char c:ch){
-        ans+=string(freq[c],c);
+        ans+=string(freq[(unsigned char)c],c);
        }
        return ans;
 
