a2580b213819af56b1a68140a5e88aad3fd09107
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-03-26 11:09:50 +0530

    Time: 0 ms (100%), Space: 9.6 MB (45.84%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n=s.size();
+        for(int i=0;i<n;i+=2*k){
+            int start=i;
+            int end=min(start+k-1,n-1);
+            while(start<end){
+                std::swap(s[start],s[end]);
+                start++;
+                end--;
+            }
+            
+        }
+        return s;
+    }
+};
