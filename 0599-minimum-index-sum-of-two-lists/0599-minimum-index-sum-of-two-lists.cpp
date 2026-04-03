d15a330d1557a506c344d5a743b4a211571341cd
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-04-04 02:08:50 +0530

    Time: 28 ms (44.07%), Space: 41.4 MB (43.6%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
@@ -11,12 +11,14 @@ public:
         int mins = INT_MAX;
 
         for(int i = 0; i < list2.size(); i++){
+            if(i > mins) break;
+
             if(mp.find(list2[i]) != mp.end()) {
                 int sum = i + mp[list2[i]];
 
                 if(sum < mins){
                     mins = sum;
-                    res.clear(); // VERY IMPORTANT
+                    res.clear();
                     res.push_back(list2[i]);
                 }
                 else if(sum == mins){
