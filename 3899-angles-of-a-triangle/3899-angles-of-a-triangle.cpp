06529004d3da851eeb55419e99f5485259bc409c
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-04-13 01:34:23 +0530

    Time: 0 ms (100%), Space: 28.9 MB (77.78%) - LeetHub

--- 3899-angles-of-a-triangle/3899-angles-of-a-triangle.cpp
+++ 3899-angles-of-a-triangle/3899-angles-of-a-triangle.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    vector<double> internalAngles(vector<int>& sides) {
-        vector<double>res;
-        double PI=acos(-1);
-        int a=sides[0];
-        int b=sides[1];
-        int c=sides[2];
-        double s=(a+b+c)/2.0;
-        if((s-a)<=0 || (s-b)<=0 || (s-c)<=0) return res;
+   vector<double> internalAngles(vector<int>& sides) {
+    vector<double> res;
 
-         double cosa=(b*b + c*c - a*a)/(2*(double)b*c);
-        double cosb=(a*a + c*c - b*b)/(2*(double)a*c);
-        double cosc=(a*a + b*b -c*c)/(2*(double)a*b);
-        double value1=acos(min(1.0,max(-1.0,cosa)));
-        double value2=acos(min(1.0,max(-1.0,cosb)));
-        double value3=acos(min(1.0,max(-1.0,cosc)));
-        res={value1*(180/PI),value2*(180/PI),value3*(180/PI)};
-        std::sort(res.begin(),res.end());
-        return res;
-    }
+    double a = sides[0];
+    double b = sides[1];
+    double c = sides[2];
+
+    if(a + b <= c || a + c <= b || b + c <= a) return res;
+
+    double cosa = (1.0*b*b + 1.0*c*c - 1.0*a*a) / (2.0*b*c);
+    double cosb = (1.0*a*a + 1.0*c*c - 1.0*b*b) / (2.0*a*c);
+    double cosc = (1.0*a*a + 1.0*b*b - 1.0*c*c) / (2.0*a*b);
+
+    cosa = min(1.0, max(-1.0, cosa));
+    cosb = min(1.0, max(-1.0, cosb));
+    cosc = min(1.0, max(-1.0, cosc));
+
+    double PI = acos(-1);
+
+    res = {
+        acos(cosa) * 180.0 / PI,
+        acos(cosb) * 180.0 / PI,
+        acos(cosc) * 180.0 / PI
+    };
+    std::sort(res.begin(),res.end());
+
+    return res;
+}
 };
