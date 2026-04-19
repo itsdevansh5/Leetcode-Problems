f47a98601eb918b92f9316a509153babfa0867e7
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-04-19 22:35:45 +0530

    Time: 0 ms (100%), Space: 7.8 MB (12.46%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     string defangIPaddr(string address) {
         string result;
-        string period="[.]";
-        string a;
-        stringstream ss(address);
-        while(getline(ss,a,'.')){
-            result+=a;
-            if(!ss.eof()) result+=period;
+        for(char c : address){
+            if(c == '.') result += "[.]";
+            else result += c;
         }
         return result;
     }
