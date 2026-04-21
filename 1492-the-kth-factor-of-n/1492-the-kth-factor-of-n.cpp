e4ceac0ef6fa24cd7729724a6a8900e42bb73660
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-04-21 22:14:48 +0530

    Time: 0 ms (100%), Space: 8 MB (53.91%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
+        vector<int> small, large;
         
+        for(int i = 1; i * i <= n; i++) {
+            if(n % i == 0) {
+                small.push_back(i);
+                if(i != n / i)
+                    large.push_back(n / i);
+            }
+        }
+        
+        // small = increasing, large = decreasing
+        if(k <= small.size()) return small[k - 1];
+        
+        k -= small.size();
+        reverse(large.begin(), large.end());
+        
+        if(k <= large.size()) return large[k - 1];
+        
+        return -1;
     }
 };
