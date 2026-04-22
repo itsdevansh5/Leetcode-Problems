c00526a0775259551def918c773d37937fbd99a5
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-04-22 23:23:40 +0530

    Time: 15 ms (36.9%), Space: 202.7 MB (79.03%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -3,19 +3,18 @@ public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n=nums.size();
         vector<int>res(n,0);
+         int mine=INT_MAX;
+        for(int i=n-1;i>=0;i--){
+            mine=min(mine,nums[i]);
+            res[i]=mine;
+        }
         int maxe=0;
         for(int i=0;i<n;i++){
             maxe=max(maxe,nums[i]);
-            res[i]=maxe;
+            if(maxe-res[i]<=k) return i;
         }
-        int mine=INT_MAX;
-        for(int i=n-1;i>=0;i--){
-            mine=min(mine,nums[i]);
-            res[i]-=mine;
-        }
-        for(int i=0;i<n;i++){
-            if(res[i]<=k) return i;
-        }
+       
+       
         return -1;
 
 
