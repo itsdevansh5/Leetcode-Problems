5de33742cdf2f8b60481682f737a994ef7ff592c
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-04-24 22:30:39 +0530

    Time: 0 ms (100%), Space: 9.1 MB (30.54%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int furthestDistanceFromOrigin(string moves) {
-        int a=0;
-        int b=0;
-        for(char t:moves){
-            if(t=='L') a++;
-            else if(t=='R') b++;
+        int l = 0, r = 0, u = 0;
+        
+        for(char c : moves){
+            if(c == 'L') l++;
+            else if(c == 'R') r++;
+            else u++;
         }
-        char p=(a>b)?'L':'R';
-        int d=0;
-        for(int i=0;i<moves.size();i++){
-            char v=(moves[i]!='_')?moves[i]:p;
-            if(v=='L') d++;
-            else if(v=='R') d--;
-        }
-        return abs(d);
+        
+        return abs(l - r) + u;
     }
 };
