fbf0fb92555784cbd3289b7df8d681d9ad86f7d4
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-05-09 08:31:46 +0530

    Time: 0 ms (100%), Space: 8.6 MB (49.16%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,20 +1,7 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x < 2) return x;
-
-        int left = 1, right = x / 2, ans = 0;
-        while (left <= right) {
-            long long mid = left + (right - left) / 2;
-            long long sq = mid * mid;
-            if (sq == x) return (int)mid;
-            if (sq < x) {
-                ans = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return ans;
+        int r=pow(x,0.5);
+        return r;
     }
 };
