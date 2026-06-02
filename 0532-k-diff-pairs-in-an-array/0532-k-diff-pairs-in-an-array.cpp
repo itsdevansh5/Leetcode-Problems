f96a724c20cd2438f03c162867d05001b722d276
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-06-02 15:11:06 +0530

    Time: 0 ms (100%), Space: 14.9 MB (91.34%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -2,17 +2,25 @@ class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
         std::sort(nums.begin(),nums.end());
-        int n=nums.size();
-        unordered_map<int,int>mp;
-        int ct=0;
         int i=0;
-        while(i<n){
-          int need=nums[i]-k;
-          if(k!=0 && mp.count(need) && !mp.count(nums[i])) ct++;
-          else if(k==0 && mp[nums[i]]==1) ct++;
-          mp[nums[i]]++;
-          i++;
+        int j=1;
+        int ct=0;
+        int n=nums.size();
+        while(i<n && j<n){
+          int diff=nums[j]-nums[i];
+          if(diff==k) {
+            ct++;
+            i++;
+          }
+          else if(diff>k) i++;
+          else j++;
+
+          while(i>0 && i<n && nums[i]==nums[i-1]) i++;
+    
+          
+          if(i==j) j++;
         }
         return ct;
+
     }
 };
