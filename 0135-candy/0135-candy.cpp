08e39f5a7af84122119832a816f17ff6d9d9a88f
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-06-12 01:45:26 +0530

    Time: 0 ms (100%), Space: 23 MB (29.13%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n=ratings.size();
-        vector<int>res(n,1);
+      int n=ratings.size();
+        vector<int>candy(n,1);
         for(int i=0;i<n-1;i++){
-            if(ratings[i]<ratings[i+1] && res[i]>=res[i+1])
-            res[i+1]=res[i]+1;
-            else if(ratings[i]>ratings[i+1] && res[i]<=res[i+1]){
-              res[i]=res[i+1]+1;
-              int a=i-1;
-              int b=i;
-              while(a>=0 && ratings[a]>ratings[b] && res[a]<=res[b]){
-                res[a]=res[b]+1;
-                a--;
-                b--;
-              }
-            } 
+          if(ratings[i]<ratings[i+1]) candy[i+1]=candy[i]+1;
         }
-        int sum=accumulate(res.begin(),res.end(),0);
-        return sum;
+        for(int i=n-1;i>0;i--){
+          if(ratings[i]<ratings[i-1]) candy[i-1]=max(candy[i]+1,candy[i-1]);
+        }
+        return accumulate(candy.begin(),candy.end(),0);
     }
 };
