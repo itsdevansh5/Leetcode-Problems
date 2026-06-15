bce79d0b11a055de8504f58e5bd7839969e53158
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-06-15 10:56:22 +0530

    Time: 4 ms (22.91%), Space: 11.8 MB (23.67%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -3,14 +3,14 @@ public:
     int longestValidParentheses(string s) {
          int n=s.size();
          stack<int>st;
+         st.push(-1);
          int maxl=0;
          int curl=0;
          for(int i=0;i<n;i++){
           char a=s[i];
-          if(!st.empty() && s[st.top()]=='(' && a==')'){
+          if(st.top()>=0 && s[st.top()]=='(' && a==')'){
             st.pop();
-            if(!st.empty()) curl=i-st.top();
-            else curl=i+1;
+            curl=i-st.top();
           }
 
           else st.push(i);
