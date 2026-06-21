de381b2f42af1518cbd395fc321d836eb6cd0080
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-06-21 22:52:15 +0530

    Time: 20 ms (74.94%), Space: 87.8 MB (7.44%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
+    vector<int>CountingSort(vector<int>&costs){
+      int n=costs.size();
+      vector<int>res(n);
+      int maxv=*max_element(costs.begin(),costs.end());
+      vector<int>freq(maxv+1);
+      for(int i:costs) freq[i]++;
+      for(int i=1;i<=maxv;i++){
+        freq[i]=freq[i]+freq[i-1];
+      }
+      for(int i=n-1;i>=0;i--){
+        res[freq[costs[i]]-1]=costs[i];
+        freq[costs[i]]--;
+      }
+      return res;
+    }
     int maxIceCream(vector<int>& costs, int coins) {
-        std::sort(costs.begin(),costs.end());
+        vector<int>cost=CountingSort(costs);
         int tot=0;
         int num=0;
-        for(int i=0;i<costs.size();i++){
-          if(tot+costs[i]>coins) break;
+        for(int i=0;i<cost.size();i++){
+          if(tot+cost[i]>coins) break;
           else {
-            tot+=costs[i];
+            tot+=cost[i];
             num++;
           }
         }
