402101c2050e678302a3800160abf32baf4718d5
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-06-22 10:03:09 +0530

    Time: 179 ms (13.98%), Space: 16.7 MB (45.22%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -21,9 +21,8 @@ public:
           for(int j=i+1;j<n-1;j++){
              int s=nums[i]+nums[j];
              int idx=lb(nums,s,j+1);
-             if(idx>j+1){
               tot+=(idx-j-1);
-             }
+            
 
           }
          }
