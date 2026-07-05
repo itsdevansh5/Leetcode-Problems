486360b08596447f791006e5eb722f6a6b65d670
Devansh Trivedi <trivedidevansh1080@gmail.com>
2026-07-05 14:53:33 +0530

    Time: 23 ms (99.17%), Space: 105.9 MB (34.81%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -3,14 +3,12 @@ public:
     int maxFrequency(vector<int>& nums, int k) {
       std::sort(nums.begin(),nums.end());
       int n=nums.size();
-        vector<long long>need(n,0);
         int j=0;
         long long curneed=0;
         int maxf=1;
         for(int i=0;i<n-1;i++){
            long long diff=nums[i+1]-nums[i];
            curneed+=(i-j+1)*diff;
-           need[i]=curneed;
 
            while(curneed>k){
             curneed-=(nums[i+1]-nums[j]);
